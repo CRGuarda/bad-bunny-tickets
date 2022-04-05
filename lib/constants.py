87a8012e8e405d77a74bbac1429b57ee47e91b7f
Carlos Guarda <carlos.guarda@qbitla.com>
2022-04-05 16:24:43 -0500

    correccion buy_button_xpath

--- lib/constants.py
+++ lib/constants.py
@@ -1,4 +1,4 @@
 """Contains constants used by the application."""
 
 TICKETS_URL = "https://teleticket.com.pe/daddy-yankee-enlima2022"
-BUY_BUTTON_XPATH = "//*[@id=\"choose-tickets-section\"]/a"
+BUY_BUTTON_XPATH = "//*[@id=\"choose-tickets-section text-center\"]/a"
